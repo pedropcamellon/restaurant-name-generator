4533062528ead09b9766282c598a90ae7e386e64
Pedro P. Camellon <pedropcamellon@gmail.com>
2025-04-24 17:06:42 -0400

    Improve error handling

--- generator.py
+++ generator.py
@@ -1,16 +1,31 @@
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
+from pydantic import ValidationError
 
 load_dotenv()
 
 
-llm = ChatGoogleGenerativeAI(
-    model="gemini-2.0-flash", convert_system_message_to_human=True, temperature=1
-)
+def get_llm_instance():
+    llm = None
+
+    try:
+        llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=1)
+
+    except Exception as e:
+        if "Your default credentials were not found" in str(e):
+            print("Error: Please set the GOOGLE_API_KEY environment variable.")
+
+    if llm is not None:
+        return llm
 
 
 def generate_restaurant_name(cuisine: str) -> str:
+    llm = get_llm_instance()
+
+    if llm is None:
+        return "Error: Please set the GOOGLE_API_KEY environment variable."
+
     system_template = "I want to open a restaurant. Suggest a good restaurant name based on the cuisine provided by the user. Response must include only the name of the restaurant."
 
     prompt_template = ChatPromptTemplate.from_messages(
@@ -19,7 +34,14 @@ def generate_restaurant_name(cuisine: str) -> str:
 
     prompt = prompt_template.invoke({"cuisine": cuisine})
 
-    response = llm.invoke(prompt)
+    try:
+        response = llm.invoke(prompt)
+
+    except ValidationError as e:
+        # If api key is invalid or missing tell the user
+        # Get e message
+        if "GOOGLE_API_KEY" in str(e):
+            return "Error: Please set the GOOGLE_API_KEY environment variable."
 
     print(f"Restaurant Name: {response.content}")
 
